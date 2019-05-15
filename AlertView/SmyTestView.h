c6f39da0f5808faadd7a84db552d17192d1ea833
mingyue <15142329586@163.com>
2019-05-15 19:14:19 +0800

    change file

---
+++ AlertView/SmyTestView.h
@@ -0,0 +1,17 @@
+//
+//  SmyTestView.h
+//  TrvokCIP
+//
+//  Created by 孙明月 on 2019/5/15.
+//  Copyright © 2019 loopeer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface SmyTestView : UIView
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ AlertView/SmyTestView.m
@@ -0,0 +1,21 @@
+//
+//  SmyTestView.m
+//  TrvokCIP
+//
+//  Created by 孙明月 on 2019/5/15.
+//  Copyright © 2019 loopeer. All rights reserved.
+//
+
+#import "SmyTestView.h"
+
+@implementation SmyTestView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
